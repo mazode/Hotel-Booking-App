84279d2388539559a2885779c418aa80da9b792b
mazode <mrmaazkhan191@gmail.com>
2025-08-29 23:17:07 +0300

    add: user schema model

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,5 +1,5 @@
-import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 
 export type UserType = {
   _id: string;
@@ -10,17 +10,29 @@ export type UserType = {
 };
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
 });
 
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 8);
   }
-  next();
 });
 
 const User = mongoose.model<UserType>("User", userSchema);
