f1fbc3682703e9cd4a18c3251869ca291be32733
mazode <mrmaazkhan191@gmail.com>
2025-09-01 17:17:06 +0300

    setup the project for deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
-import SignIn from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 
 const App = () => {
   return (
