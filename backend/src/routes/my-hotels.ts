1238d5e5350f29a1de685ccfc03f091617653915
mazode <mrmaazkhan191@gmail.com>
2025-09-01 22:58:49 +0300

    add: my-hotel route, hotel model schema

---
+++ backend/src/routes/my-hotels.ts
@@ -0,0 +1,70 @@
+import express, { Request, Response } from "express";
+import multer from "multer";
+import cloudinary from "cloudinary";
+import Hotel, { HotelType } from "../models/hotel";
+import verifyToken from "../middlewares/auth";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
+  },
+});
+
+// my-hotels route
+router.post(
+  "/",
+  verifyToken,
+  [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("city").notEmpty().withMessage("City is required"),
+    body("country").notEmpty().withMessage("Country is required"),
+    body("description").notEmpty().withMessage("Description is required"),
+    body("type").notEmpty().withMessage("Type is required"),
+    body("pricePerNight")
+      .notEmpty()
+      .isNumeric()
+      .withMessage("Price Per Night is required, and must be a number"),
+    body("facilities")
+      .notEmpty()
+      .isArray()
+      .withMessage("Facilities are required"),
+    body("imageUrls").notEmpty().withMessage("Image URLs are required"),
+  ],
+  upload.array("imageFiles", 6),
+  async (req: Request, res: Response) => {
+    try {
+      const imageFiles = req.files as Express.Multer.File[];
+      const newHotel: HotelType = req.body;
+
+      // Upload the images to the cloudinary
+      const uploadPromises = imageFiles.map(async (image) => {
+        const b64 = Buffer.from(image.buffer).toString("base64");
+        let dataURI = "data:" + image.mimetype + ";base64," + b64;
+        const response = await cloudinary.v2.uploader.upload(dataURI);
+        return response.url;
+      });
+
+      // Add URLs to the new hotel upon successful upload
+      const imageUrls = await Promise.all(uploadPromises);
+      newHotel.imageUrls = imageUrls;
+      newHotel.lastUpdated = new Date();
+      newHotel.userId = req.userId;
+
+      // Save the new hotel in database (Hotel model schema)
+      const hotel = new Hotel(newHotel);
+      await hotel.save();
+
+      res.status(201).send(hotel);
+    } catch (error) {
+      console.log("Error creating hotel: ", error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+);
+
+export default router;
